51c050a67ea91645b3a8273d25047a0656fdd046
Rahul M <getmrahul@gmail.com>
2014-12-20 13:18:55 +0530

    UI update

--- app.py
+++ app.py
@@ -9,12 +9,25 @@ from werkzeug import secure_filename
 app = Flask(__name__)
 app.debug = True
 app.secret_key = os.urandom(25)
-
+slack_id = '3259978903.3264616612'
+slack_sec = '0387e1dc50dd6e816d855270bdadb339'
 
 #Routes
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/auth')
+def auth():
+    try:
+        code = request.args.get('code')
+        code = Markup(code).striptags()
+        url = 'https://slack.com/api/oauth.access?'+'code='+code+'&client_id=3111086527.3259178475&client_secret=fd3641ce49e6e2fa94a0bffafd3b57ad'
+        resp = json.load(urllib2.urlopen(url))
+        return resp
+
+    except Exception as exp:
+        return render_template('error.html')
 
 
 
