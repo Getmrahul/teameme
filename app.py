efb85dfa6611751e1fb9c8cdf1d7a0eddd572f69
Rahul M <getmrahul@gmail.com>
2014-12-20 14:36:04 +0530

    UI update

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import db
 
 #App Settings
 app = Flask(__name__)
-app.debug = True
 app.secret_key = os.urandom(25)
 slack_id = '3259978903.3264616612'
 slack_sec = '0387e1dc50dd6e816d855270bdadb339'
@@ -23,22 +22,19 @@ def index():
 
 @app.route('/auth')
 def auth():
-    try:
-        code = request.args.get('code')
-        response = urllib2.urlopen('https://slack.com/api/oauth.access?code='+code+'&client_id='+slack_id+'&client_secret='+slack_sec)
-        data = json.load(response)
-        auth_code = data["access_token"].encode('utf-8')
-        response = urllib2.urlopen('https://slack.com/api/auth.test?token='+auth_code)
-        data = json.load(response)
-        tid = data["team_id"]
-        uid = data["user_id"]
-        records = db_obj.connect(tid, uid)
-        if not records:
-            return render_template('join.html', token = auth_code)
-        else:
-            return "Existing user"
-    except Exception as exp:
-        return exp
+    code = request.args.get('code')
+    response = urllib2.urlopen('https://slack.com/api/oauth.access?code='+code+'&client_id='+slack_id+'&client_secret='+slack_sec)
+    data = json.load(response)
+    auth_code = data["access_token"].encode('utf-8')
+    response = urllib2.urlopen('https://slack.com/api/auth.test?token='+auth_code)
+    data = json.load(response)
+    tid = data["team_id"]
+    uid = data["user_id"]
+    records = db_obj.connect(tid, uid)
+    if not records:
+        return render_template('join.html', token = auth_code)
+    else:
+        return "Existing user"
 
 
 
